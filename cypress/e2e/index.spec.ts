2c8ce96afbd5bcbc2a45aeb8bd2b204901cefef8
Antonio Galindo <galindosvq@gmail.com>
2022-09-08 10:38:38 +0200

    rebuild project

---
+++ cypress/e2e/index.spec.ts
@@ -0,0 +1,10 @@
+describe('Basic flow', () => {
+	beforeEach(() => {
+		cy.viewport('macbook-13')
+	})
+
+	// it('Should redirect to gallery when trying to access a invalid fruit', () => {
+	// 	cy.visit('/cypress')
+	// 	cy.location('pathname').should('eq', '/')
+	// })
+})
